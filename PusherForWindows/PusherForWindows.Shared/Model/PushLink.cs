680db146a2b1ac3ccb342323cb5cf92f57af8b25
Andrea Pivetta <vanpivix@gmail.com>
2015-04-25 00:33:18 +0200

    More things displayed but in the wrong way

--- PusherForWindows/PusherForWindows.Shared/Model/PushLink.cs
+++ PusherForWindows/PusherForWindows.Shared/Model/PushLink.cs
@@ -6,12 +6,12 @@ namespace PusherForWindows.Model
 {
     class PushLink : Push
     {
-        public Uri url { get; set; }
+        public Uri URL { get; set; }
 
-        public PushLink(string Iden, TYPES Type, string Title, long Created, long Modified, string url) 
+        public PushLink(string Iden, TYPES Type, string Title, long Created, long Modified, string url)
             : base(Iden, Type, Title, Created, Modified)
         {
-            this.url = new Uri(url);
+            this.URL = new Uri(url);
         }
     }
 }
